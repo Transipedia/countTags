755dc99be5c759cbdb5f68af7ce8105622b5623d
Anthony Boureux <Anthony.Boureux@univ-montp2.fr>
2018-03-26 00:19:37 +0200

    Add GPL copyrigth to each files

--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,33 @@
+/******************************************************************************\
+*                                                                              *
+*  Copyright © 2015-2018 -- IRMB/INSERM                                        *
+*                           (Institute for Regenerative Medicine & Biotherapy  *
+*                           Institut National de la Santé et de la Recherche   *
+*                           Médicale)                                          *
+*                                                                              *
+*  Copyright (C) 2015-2018  Jérôme Audoux                                      *
+*                                                                              *
+*  This file is part of countTags program.                                     *
+*                                                                              *
+*  The purpose of countTags is to count occurences of few tags in large set of *
+*  fastq files.                                                                *
+*                                                                              *
+*   countTags is free software: you can redistribute it and/or modify          *
+*   it under the terms of the GNU General Public License as published by       *
+*   the Free Software Foundation, either version 3 of the License, or          *
+*   (at your option) any later version.                                        *
+*                                                                              *
+*   countTags is distributed in the hope that it will be useful,               *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+*   GNU General Public License for more details.                               *
+*                                                                              *
+*   You should have received a copy of the GNU General Public License          *
+*   along with countTags program. If not, see <http://www.gnu.org/licenses/>.  *
+*                                                                              *
+*                                                                              *
+*******************************************************************************/
+
 #ifndef UTILS_H
 #define UTILS_H
 
